1e408b8301ea739bce055421ad7a3ee30c87bdb7
Azeez Samad <seshp@ymail.com>
2022-05-11 20:30:42 +0100

    destructure movies to a new file

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {  FaHeart, FaRegHeart } from 'react-icons/fa'
+
+import Movie from "./Movie";
 
 const Row = ({ title, fetchLink }) => {
     const [movies, setMovies] = useState([]);
-    const [like, setLike] = useState(false);
+
 
     useEffect(() => {
       axios.get(fetchLink).then((response) => {
@@ -20,13 +21,7 @@ const Row = ({ title, fetchLink }) => {
         <div className="relative flex items-center">
             <div id={'slider'}>
                 {movies?.map((item, id)=> (
-                    <div className=" w-[160px] sm:w-[200px] md:w-[280px] lg:w-[280px] inline-block relative cursor-pointer"> 
-                        <img className="text-white w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} /> 
-                        <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                            <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center text-center h-full">{item?.title}</p>
-                            <p> {like ? <FaHeart className="absolute top-4 left-4 text-gray-300"/> : <FaRegHeart className="absolute top-4 left-4text-gray-300" />}</p>
-                        </div>
-                    </div>
+                   <Movie key={id} item={item}/>
                 ))}
             </div>
         </div>
