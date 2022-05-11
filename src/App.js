09ce795da5c6d9fcede76588550e0a406d2ef12e
Azeez Samad <seshp@ymail.com>
2022-05-11 14:48:17 +0100

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+const { Routes, Route } = require("react-router-dom");
 const { default: Navbar } = require("./components/Navbar");
+const { default: Home } = require("./pages/Home");
 
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
