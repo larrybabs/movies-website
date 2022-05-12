13727401c3be796ee8769519cbc0af86e52c5af9
Azeez Samad <seshp@ymail.com>
2022-05-12 13:54:36 +0100

    add firebase connections

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { AuthContextProvider } from "./context/AuthContext";
+
 const { Routes, Route } = require("react-router-dom");
 const { default: Navbar } = require("./components/Navbar");
 const { default: Home } = require("./pages/Home");
 
-
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </> 
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
